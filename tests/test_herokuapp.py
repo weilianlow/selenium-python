ca831e73287f6cfb94476d8174d56e60f18fce9a
weilianlow <weilian.low88@gmail.com>
2022-08-01 11:31:07 +0800

    added new tests

--- tests/test_herokuapp.py
+++ tests/test_herokuapp.py
@@ -5,12 +5,23 @@ from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver import ActionChains
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support.select import Select
 from requests import get
+import getpass
 
 
 class TestElements(unittest.TestCase):
     def setUp(self):
+        """
+        options = Options()
+        options.add_experimental_option("prefs", {
+            "download.default_directory": f"/Users/{getpass.getuser()}/Downloads",
+            "download.prompt_for_download": False,
+            "download.directory_upgrade": True,
+            "safebrowsing.enabled": True
+        })
+        """
         self.driver = Driver('Chrome').get_driver()
         self.driver.implicitly_wait(10)
         self.driver.maximize_window()
@@ -56,9 +67,33 @@ class TestElements(unittest.TestCase):
         select.select_by_visible_text('Option 2')
         assert 'Option 2' == select.first_selected_option.text
 
-    def test_explicit_wait(self):
+    def test_remove_element_and_add_back(self):
         self.url('dynamic_controls')
-        for val in [('#checkbox > input[type=checkbox]', EC.staleness_of), ('#checkbox-example > button', EC.visibility_of)]:
+        for val in [('#checkbox > input[type=checkbox]', EC.staleness_of),
+                    ('#checkbox-example > button', EC.visibility_of)]:
             self.driver.find_element(By.CSS_SELECTOR, '#checkbox-example > button').click()
             cb = self.driver.find_element(By.CSS_SELECTOR, val[0])
             WebDriverWait(self.driver, 10).until(val[1](cb))
+
+    def test_hide_element(self):
+        self.url('download')
+        btn = self.driver.find_element(By.CSS_SELECTOR, '#content > div > a:nth-child(2)')
+        btn.click()
+        WebDriverWait(self.driver, 10).until(EC.invisibility_of_element(btn))
+
+    def test_download(self):
+        self.url('downloads')
+
+    def test_iframe(self):
+        self.url('iframe')
+        self.driver.switch_to.frame('mce_0_ifr')
+        content = self.driver.find_element(By.CSS_SELECTOR, '#tinymce > p')
+        content.clear()
+        content.send_keys('Hello')  # this triggers a change to the element, resulting it to be stale
+        assert 'Hello' == self.driver.find_element(By.CSS_SELECTOR, '#tinymce > p').text
+
+    def test_hover(self):
+        self.url('hovers')
+        actionChains = ActionChains(self.driver)
+        actionChains.move_to_element(self.driver.find_element(By.CSS_SELECTOR, '#content > div > div:nth-child(3) > img')).perform()
+        assert 'View profile' == self.driver.find_element(By.XPATH, '//a[text()="View profile"]').text
--- utils/driver.py
+++ utils/driver.py
@@ -2,12 +2,15 @@ import selenium.webdriver
 
 
 class Driver:
-    def __init__(self, browser):
+    def __init__(self, browser, options=None):
         self.driver = None
         if browser == 'Firefox':
             self.driver = selenium.webdriver.Firefox()
         elif browser == 'Chrome':
-            self.driver = selenium.webdriver.Chrome()
+            if options:
+                self.driver = selenium.webdriver.Chrome(options=options)
+            else:
+                self.driver = selenium.webdriver.Chrome()
         elif browser == 'Headless Chrome':
             opts = selenium.webdriver.ChromeOptions()
             opts.add_argument('headless')
